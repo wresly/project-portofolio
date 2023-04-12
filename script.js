3f6dac889f92b643c449c2679d2c3bee865f1deb
wresly <wreslyroyale1717@gmail.com>
2023-04-12 11:14:05 +0800

    message

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const buttonsubmit = document.getElementById("submit");
+const alertwarning = document.getElementById("warning");
+alertwarning.style.display = "none"
 const alert = document.getElementById("alert");
 alert.style.display = "none"
 
 buttonsubmit.addEventListener("click", function () {
-    const pengirim = document.getElementById("txtPengirim").value;
-    const jeniskelamin = document.getElementById("jenisKelamin").value;
-    const pesan = document.getElementById("txtPesan").value;
+    alert.style.display = "none"
+    alertwarning.style.display = "none"
+    const pengirim = document.getElementById("txtPengirim");
+    const jeniskelamin = document.getElementById("jenisKelamin");
+    const pesan = document.getElementById("txtPesan");
     const result = document.getElementById("txtResultPesan");
 
+    if (pengirim.value === "") {
+        alertwarning.innerHTML = "nama pengirim tidak boleh kosong"
+        alertwarning.style.display = "block"
+        pengirim.focus()
+        return 0;
+    }
+
+    if (jeniskelamin.value === "default") {
+        alertwarning.innerHTML = "jenis kelamin tidak boleh kosong"
+        alertwarning.style.display = "block"
+        jeniskelamin.focus()
+        return 0;
+    }
+
+    if (pesan.value === "") {
+        alertwarning.innerHTML = "pesan tidak boleh kosong"
+        alertwarning.style.display = "block"
+        pesan.focus()
+        return 0;
+    }
+
     const resulttext = `
-    Nama: ${pengirim}
-    Jenis Kelamin: ${jeniskelamin}
-    Pesan: ${pesan}
+    Nama : ${pengirim.value}
+    Jenis Kelamin : ${jeniskelamin.value}
+    Pesan : ${pesan.value}
     `;
     result.value += resulttext;
     alert.style.display = "block"
